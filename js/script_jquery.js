4f1dac1d018b4c521c5572704943d120ed6348e3
HenryRabelo <henry.rabelo@outlook.com>
2020-06-16 15:30:39 -0300

    Fix secundário do carrinho (2/2).

--- js/script_jquery.js
+++ js/script_jquery.js
@@ -3,81 +3,95 @@ $(document).ready(function(){
 	
 	var conteudo = document.getElementById('item-list');
 	var show=parseInt('0');
+
 	$(document).on('click','#food-cart',function(){
-       if(show != 0){
-        $('#cart').show();
-        //conteudo.innerHTML= vetor[i-1];
-        if(i==0){
-        	conteudo.innerHTML="<h4> Ainda não temos uma receita ao carrinho! </h4>";
-        } else {
+    	if (show != 0) {
+        	$('#cart').show();
+        	//conteudo.innerHTML= vetor[i-1];
+        	if (i==0) {
+        		conteudo.innerHTML="<h4> Ainda não temos uma receita no carrinho! </h4>";
+        	} else {
 
-        var aux="<tr><td> Seu carrinho: </td></tr>";
-        var j= parseInt('0');
-        
-        for(j=0;j<i;j++){
-        	aux= aux + ("<tr><td><img class='imagem-produto' src=assets/img/produtos/"+vetor[j]+".jpeg></td><td><p>"+vetor[j]+"</p></td></tr>");
-        }
-        conteudo.innerHTML=aux;
+        		var cartao = "";
+        		var j = parseInt('0');
 
-    }
-        show--;
-	}
+        		for(j = 0; j < i; j++) {
 
-    else{
-    	 $('#cart').hide();
-    	 show++;
-    }
-       });
+        			cartao = cartao + ('<tr><td> \
+        				<div id="item" class="card mb-3 h-100"> \
+        					<div id="item-title" class="card-header"> <p>'+vetor[j]+'</p> </div> \
+        					<img id="item-img" src="assets/img/produtos/'+vetor[j]+'.jpeg" class="card-img-top" alt="..." /> \
+        					<div class="card-footer"> \
+        						<button type="button" class="btn btn-info"> Ver Receita </button> \
+        						<button type="button" class="btn btn-info"> Remover </button> \
+        					</div> \
+        				</div> \
+        			</td></tr>');
+
+        		}
+
+        		conteudo.innerHTML = cartao;
+
+    		}
+
+        	show--;
+
+		} else {
+    		$('#cart').hide();
+    		show++;
+    	}
+	});
     
 	$(document).keypress(function(e){
 	//	var t = e.which;
 		if(e.which==13)
 			$('#Search').click();
-		});
+	});
 
 	$(document).on('click','.close', function(){
 		$('#cart').hide();
 	});
 
-	var card= new Array();
-	var i=parseInt('0');
+	var card = new Array();
+	var i = parseInt('0');
 	var vetor = new Array();
 
 	$(document).on('click','#Search',function(e){
 		var busca =document.getElementById('search-input').value;
-    switch(busca){
-	case "macarrao":
-	vetor[i]="Macarrao";
-	alert("Inseriu");
-	i++;
-	//alert("tem");
-	break;
+    	switch(busca){
+		case "macarrao":
+			vetor[i]="Macarrao";
+			alert("Inseriu");
+			i++;
+			//alert("tem");
+		break;
 
-	case "batata":
-	vetor[i]="Batata";
-	alert("Inseriu");
-	i++;
-	//alert("tem");
-	break;
+		case "batata":
+			vetor[i]="Batata";
+			alert("Inseriu");
+			i++;
+			//alert("tem");
+		break;
 
-	case "pudim":
-    vetor[i]="Pudim";
-    alert("Inseriu");
-	i++;
-	//alert("tem");
-	break;
+		case "pudim":
+    		vetor[i]="Pudim";
+    		alert("Inseriu");
+			i++;
+			//alert("tem");
+		break;
 
-	case "pizza":
-    vetor[i]="Pizza";
-    alert("Inseriu");
-	i++;
-	//alert("tem");
-	break;
+		case "pizza":
+    		vetor[i]="Pizza";
+    		alert("Inseriu");
+			i++;
+			//alert("tem");
+		break;
 
-	default:
-	//alert("Nenhum produto com este nome");
+		default:
+			//alert("Nenhum produto com este nome");
+		break;
 	
-}
+		}
 
 
 	});
