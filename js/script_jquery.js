4c6b95786203cda13e99058eb3b56f400ee9c0b5
HenryRabelo <henry.rabelo@outlook.com>
2020-06-16 17:00:01 -0300

    Fix de legibilidade

--- js/script_jquery.js
+++ js/script_jquery.js
@@ -7,43 +7,43 @@ $(document).ready(function(){
 
 	$(document).on('click','#food-cart',function(){
 
-        	//conteudo.innerHTML= vetor[i-1];
-        	if (i==0) {
-        		conteudo.innerHTML="<h4> Ainda não temos uma receita no carrinho! </h4>";
-        	} else {
+        //conteudo.innerHTML= vetor[i-1];
+        if (i == 0) {
+        	conteudo.innerHTML="<h4> Ainda não temos uma receita no carrinho! </h4>";
+        } else {
 
-        		var cartao = "";
-        		var j = parseInt('0');
+        	var cartao = "";
+        	var j = parseInt('0');
 
-        		for(j = 0; j < i; j++) {
+        	for(j = 0; j < i; j++) {
 
-        			cartao = cartao + ('<tr><td> \
-        				<div id="item" class="card mb-3 h-100"> \
-        					<div id="item-title" class="card-header"> <p>'+vetor[j]+'</p> </div> \
-        					<img id="item-img" src="assets/img/produtos/'+vetor[j]+'.jpeg" class="card-img-top" alt="..." /> \
-        					<div class="card-footer"> \
-        						<button type="button" class="btn btn-info"> Ver Receita </button> \
-        						<button type="button" class="btn btn-info"> Remover </button> \
-        					</div> \
+        		cartao = cartao + ('<tr><td> \
+        			<div id="item" class="card mb-3 h-100"> \
+        				<div id="item-title" class="card-header"> <p>'+vetor[j]+'</p> </div> \
+        				<img id="item-img" src="assets/img/produtos/'+vetor[j]+'.jpeg" class="card-img-top" alt="..." /> \
+        				<div class="card-footer"> \
+        					<button type="button" class="btn btn-info"> Ver Receita </button> \
+        					<button type="button" class="btn btn-info"> Remover </button> \
         				</div> \
-        			</td></tr>');
-
-        		}
-
-        		conteudo.innerHTML = cartao;
+        			</div> \
+        		</td></tr>');
 
     		}
+
+    		conteudo.innerHTML = cartao;
+
+    	}
 
 	});
     
 	$(document).keypress(function(e){
 	//	var t = e.which;
-		if(e.which==13)
+		if(e.which == 13)
 			$('#Search').click();
 	});
 
 	$(document).on('click','#Search',function(e){
-		var busca =document.getElementById('search-input').value;
+		var busca = document.getElementById('search-input').value;
     	switch(busca){
 		case "macarrao":
 			vetor[i]="Macarrao";
